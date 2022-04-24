9dc8aa417910242acb1652767c3f49c43bfae9ea
mayank18garg <mayank.ind18@gmail.com>
2022-04-24 02:55:43 -0700

    UnknowAttrType in BPIterator

---
+++ src/BPIterator/UnknowAttrType.java
@@ -0,0 +1,8 @@
+package BPIterator;
+
+import chainexception.ChainException;
+
+public class UnknowAttrType extends ChainException {
+    public UnknowAttrType(String s){super(null,s);}
+  public UnknowAttrType(Exception prev, String s){super(prev,s);}
+}
